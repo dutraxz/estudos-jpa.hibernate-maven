853acc7993257a448d7b19d36c4e1351b52f23fb
dutraxz <victoreduardo0896@gmail.com>
2025-11-17 15:10:43 -0300

    feat | Pessoa.java: Adicionando Annotations

--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -1,11 +1,17 @@
 package dominio;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
 
-
-    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
