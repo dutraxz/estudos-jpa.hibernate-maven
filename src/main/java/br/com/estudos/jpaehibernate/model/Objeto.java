a7b834a75db35f0f7438b14c1a0775b89286606e
dutraxz <victoreduardo0896@gmail.com>
2025-11-24 00:01:50 -0300

    feat|model: Adicionando anotações aos atributos

--- src/main/java/br/com/estudos/jpaehibernate/model/Objeto.java
+++ src/main/java/br/com/estudos/jpaehibernate/model/Objeto.java
@@ -2,49 +2,50 @@ package br.com.estudos.jpaehibernate.model;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name="objeto")
-public class Objeto{
+public class Objeto implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    @Column
+    @Column(nullable = false)
     private String nome;
-    @Column
+    @Column(nullable = false)
     private String descricao;
+    @Column(nullable = false, unique = true)
+    private Integer lacre;
 
-    public Integer getId() {
-        return id;
-    }
+    public Objeto(){} //Obrigatório pelo JPA
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
+    public Objeto(String nome, String descricao, Integer lacre) {
         this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
         this.descricao = descricao;
+        this.lacre = lacre;
     }
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getDescricao() {return descricao;}
+    public void setDescricao(String descricao) {this.descricao = descricao;}
+
+    public Integer getLacre() {return lacre;}
+    public void setLacre(Integer lacre) {this.lacre = lacre;}
 
     @Override
     public String toString() {
         return "Pessoa{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
-                ", email='" + descricao + '\'' +
+                ", descrição ='" + descricao + '\'' +
+                ", lacre'" + lacre + '\'' +
                 '}';
     }
 }
